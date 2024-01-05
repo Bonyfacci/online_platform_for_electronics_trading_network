82ed0b21403971d079330f716945eb0880ff17f8
Сергей Платонов <platonovpochta@gmail.com>
2024-01-05 02:47:15 +0100

    add test Product add layouts html

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from product.models import Product
+
+
+class ProductTestCase(APITestCase):
+    """
+    Для тестирования продукта (Product)
+    """
+
+    def setUp(self):
+        self.product = Product.objects.create(
+            name='TestProduct',
+            model='TestModel',
+            date_of_release='2023-01-01'
+        )
+
+    def test_get_product(self):
+        """
+        Тест ProductListView
+        """
+        response = self.client.get(
+            reverse('product:products')
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            Product.objects.all().count(),
+            1
+        )
+
+        self.assertContains(
+            response,
+            'TestProduct'
+        )
